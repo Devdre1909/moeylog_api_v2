8c8d575a28b3537a861703d0926fec5713ada6fe
Adegoke Temitope <adegoketemitope1909@gmail.com>
2020-03-25 16:09:22 +0100

    model for debit and credit transaction

---
+++ server/models/credit.js
@@ -0,0 +1,44 @@
+const mongoose = require('mongoose');
+let Schema = mongoose.Schema;
+
+let date = new Date();
+
+let date_now = date.toDateString();
+let time_now = date.toLocaleTimeString();
+
+let Credit = new Schema({
+    user_id: {
+        type: String,
+        required: [true, 'user id is required'],
+    },
+    amount: {
+        type: Int32Array,
+        required: [true, 'amount is required']
+    },
+    description: {
+        type: String,
+        required: [true, 'description of credit is required']
+    },
+    comment: {
+        type: String,
+        required: [false],
+        default: "No comment"
+    },
+    can_edit: {
+        type: Boolean,
+        required: [false],
+        default: false
+    },
+    time: {
+        type: String,
+        default: time_now
+    },
+    time: {
+        type: Date,
+        default: date_now
+    },
+}, {
+    timestamps: true
+});
+
+module.exports = mongoose.model('Credit', Credit)
