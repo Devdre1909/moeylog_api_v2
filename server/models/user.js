5958eca1ae250182aa73ecea86721a6125d7adb0
Adegoke Temitope <adegoketemitope1909@gmail.com>
2020-03-25 17:39:09 +0100

    update Schema to include currency

--- server/models/user.js
+++ server/models/user.js
@@ -1,6 +1,13 @@
 const mongoose = require('mongoose');
 let Schema = mongoose.Schema;
 
+const ObjCurrency = JSON.parse(fs.readFileSync('../../currency.json'));
+let currency = []
+
+for (const c in ObjCurrency) {
+    currency.push(c);
+}
+
 let User = new Schema({
     name: {
         type: String,
@@ -16,6 +23,14 @@ let User = new Schema({
     password: {
         type: String,
         required: [true, 'password is required']
+    },
+    currency: {
+        type: String,
+        required: [true, 'currency is required'],
+        enum: {
+            values: currency,
+            message: 'give access and turn on location/pick form the list'
+        }
     }
 }, {
     timestamps: true
