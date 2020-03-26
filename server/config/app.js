db9be42177480733201e969117e7e7cd7693608b
Adegoke Temitope <adegoketemitope1909@gmail.com>
2020-03-26 15:57:34 +0100

    update 404 and 505 error handling

--- server/config/app.js
+++ server/config/app.js
@@ -1,92 +1,104 @@
-const express = require('express');
-const bodyParser = require('body-parser');
-const expressValidator = require('express-validator');
-const chalk = require('chalk');
-const dotenv = require('dotenv');
-const helmet = require('helmet');
-const cors = require('cors');
-const mongoSanitize = require('express-mongo-sanitize');
-const xss = require('xss-clean');
-const hpp = require('hpp');
-const connectDB = require('./connect_db');
-const path = require('path');
-const morgan = require('morgan');
+const express = require("express");
+const bodyParser = require("body-parser");
+const expressValidator = require("express-validator");
+const chalk = require("chalk");
+const dotenv = require("dotenv");
+const helmet = require("helmet");
+const cors = require("cors");
+const mongoSanitize = require("express-mongo-sanitize");
+const xss = require("xss-clean");
+const hpp = require("hpp");
+const connectDB = require("./connect_db");
+const path = require("path");
+const morgan = require("morgan");
+const httpStatusCode = require("http-status-codes");
 
 module.exports = () => {
-    let server = express();
-    let create;
-    let start;
+  let server = express();
+  let create;
+  let start;
 
-    create = () => {
-        let routes = require('../routes');
+  create = () => {
+    let routes = require("../routes");
 
-        // handle uncaught exception
-        process.on('uncaughtException', err => {
-            console.log(chalk.red(`uncaught exception: ${err.stack}`));
-            process.exit(1);
-        });
+    // handle uncaught exception
+    process.on("uncaughtException", err => {
+      console.log(chalk.red(`uncaught exception: ${err.stack}`));
+      process.exit(1);
+    });
 
-        dotenv.config({
-            path: `${path.join(__dirname,'/env/config.env')}`
-        })
+    dotenv.config({
+      path: `${path.join(__dirname, "/env/config.env")}`
+    });
 
-        //set all server parameters
-        server.set('env', process.env.NODE_ENV);
-        server.set('port', process.env.PORT);
-        server.set('hostname', process.env.HOSTNAME)
+    //set all server parameters
+    server.set("env", process.env.NODE_ENV);
+    server.set("port", process.env.PORT);
+    server.set("hostname", process.env.HOSTNAME);
 
-        // add middleware 
-        server.use(helmet());
-        server.use(cors());
-        server.use(bodyParser.json());
-        //server.use(expressValidator);
-        server.use(
-            bodyParser.urlencoded({
-                extended: true
-            })
+    // add middleware
+    server.use(helmet());
+    server.use(cors());
+    server.use(bodyParser.json());
+    //server.use(expressValidator);
+    server.use(
+      bodyParser.urlencoded({
+        extended: true
+      })
+    );
+
+    //connect db
+    connectDB();
+
+    if (server.get("env") === "development") {
+      server.use(morgan("tiny"));
+    }
+
+    server.use(mongoSanitize());
+    server.use(xss());
+    server.use(hpp());
+
+    //set up routes
+    routes.init(server);
+
+    // 404 Error Handler. Request not found.
+    server.use((req, res, next) => {
+      res.status(httpStatusCode.BAD_REQUEST).json({
+        error: [
+          {
+            msg: "invalid endpoint. kindly check documentation"
+          }
+        ]
+      });
+    });
+
+    // 505 Error Handler
+    server.use((err, req, res, next) => {
+      console.error(chalk.red(err.stack));
+      res.status(httpStatusCode.INTERNAL_SERVER_ERROR).json({
+        error: [
+          {
+            msg: "Server on available"
+          }
+        ]
+      });
+    });
+  };
+
+  start = () => {
+    let hostname = server.get("hostname");
+    let port = server.get("port") || 5000;
+    server.listen(server.get("port"), () => {
+      console.log(
+        chalk.green.bold(
+          `Express server listening on - https://${hostname}:${port}`
         )
+      );
+    });
+  };
 
-        //connect db
-        connectDB();
-
-        if (server.get('env') === 'development') {
-            server.use(morgan('tiny'));
-        }
-
-        server.use(mongoSanitize());
-        server.use(xss());
-        server.use(hpp());
-
-        //set up routes
-        routes.init(server);
-
-        // 404 Error Handler. Request not found.
-        app.use((req, res, next) => {
-            res.status(httpStatusCode.BAD_REQUEST).json({
-                err: "wrong/unavailable endpoint",
-                context: "Good news, you are where you want to be. Bad News, you are kinda lost"
-            });
-        })
-
-        // 505 Error Handler
-        app.use((err, req, res, next) => {
-            console.error(chalk.red(err.stack));
-            res.sendFile(path.join(__dirname, '../public/505.html'));
-        })
-
-    }
-
-    start = () => {
-        let hostname = server.get('hostname');
-        let port = server.get('port') || 5000;
-        server.listen(server.get('port'), () => {
-            console.log(chalk.green.bold(`Express server listening on - https://${hostname}:${port}`))
-        })
-    }
-
-    return {
-        create,
-        start
-    }
-
-}
+  return {
+    create,
+    start
+  };
+};
