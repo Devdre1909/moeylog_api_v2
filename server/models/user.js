1e1b9b2e9cd9b3e58841d5a5251170b3a47fd522
Adegoke Temitope <adegoketemitope1909@gmail.com>
2020-03-26 16:00:47 +0100

    debug readfileSynce error

--- server/models/user.js
+++ server/models/user.js
@@ -1,39 +1,47 @@
-const mongoose = require('mongoose');
+const mongoose = require("mongoose");
+const fs = require("fs");
+const path = require("path");
+
 let Schema = mongoose.Schema;
 
-const ObjCurrency = JSON.parse(fs.readFileSync('../../currency.json'));
-let currency = []
+const ObjCurrency = JSON.parse(
+  fs.readFileSync(`${path.join(__dirname, "../../currency.json")}`)
+);
+let currency = [];
 
 for (const c in ObjCurrency) {
-    currency.push(c);
+  currency.push(c.toLowerCase());
 }
 
-let User = new Schema({
+let User = new Schema(
+  {
     name: {
-        type: String,
-        required: [true, 'name is required'],
-        lowercase: true
+      type: String,
+      required: [true, "name is required"],
+      lowercase: true
     },
     email: {
-        type: String,
-        required: [true, 'email is required'],
-        lowercase: true,
-        unique: true
+      type: String,
+      required: [true, "email is required"],
+      lowercase: true,
+      unique: true
     },
     password: {
-        type: String,
-        required: [true, 'password is required']
+      type: String,
+      required: [true, "password is required"]
     },
     currency: {
-        type: String,
-        required: [true, 'currency is required'],
-        enum: {
-            values: currency,
-            message: 'give access and turn on location/pick form the list'
-        }
+      type: String,
+      required: [true, "currency is required"],
+      enum: {
+        values: currency,
+        message: "give access and turn on location/pick form the list"
+      }
     }
-}, {
+  },
+  {
     timestamps: true
-});
+  }
+);
 
-module.exports = mongoose.model('User', User);
+module.exports = mongoose.model("User", User);
