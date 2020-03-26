61d7708a1d5b57fd6c58b6ba21c75b0022722d67
Adegoke Temitope <adegoketemitope1909@gmail.com>
2020-03-26 15:59:15 +0100

    comment out router for credit and debit

--- server/routes/apis/v1.js
+++ server/routes/apis/v1.js
@@ -1,14 +1,15 @@
-const express = require('express');
+const express = require("express");
 let router = express.Router();
 
-const userController = require('../../controllers/apis/v1/users');
-const authController = require('../../controllers/apis/v1/auth');
-const debitController = require('../../controllers/apis/v1/debit');
-const creditController = require('../../controllers/apis/v1/credit');
+const userController = require("../../controllers/apis/v1/user");
+const authController = require("../../controllers/apis/v1/auth");
+//TODO route for debit and credit
+//const debitController = require("../../controllers/apis/v1/debit");
+//const creditController = require("../../controllers/apis/v1/credit");
 
-router.use('/users', userController);
-router.use('/auth', authController);
-router.use('/credit', creditController);
-router.use('/debit', debitController);
+router.use("/users", userController);
+router.use("/auth", authController);
+//router.use("/credit", creditController);
+//router.use("/debit", debitController);
 
 module.exports = router;
