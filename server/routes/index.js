2587d6451cb23c930b77d6dfc7485bca9b4c1fa9
Adegoke Temitope <adegoketemitope1909@gmail.com>
2020-03-26 16:00:02 +0100

    format file

--- server/routes/index.js
+++ server/routes/index.js
@@ -1,15 +1,15 @@
-const chalk = require('chalk');
+const chalk = require("chalk");
 
-const apiRoutes = require('./apis');
+const apiRoutes = require("./apis");
 
-const init = (server) => {
-    server.get('*', (req, res, next) => {
-        console.log(chalk.gray(`Request maded to ${chalk.bold(req.originalUrl)}`));
-        return next();
-    })
-    server.use('/api', apiRoutes);
-}
+const init = server => {
+  server.get("*", (req, res, next) => {
+    console.log(chalk.gray(`Request made to ${chalk.bold(req.originalUrl)}`));
+    return next();
+  });
+  server.use("/api", apiRoutes);
+};
 
 module.exports = {
-    init
-}
+  init
+};
