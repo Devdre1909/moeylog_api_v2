45b1ff552a22005a88b475d804125b1a9077fda0
Adegoke Temitope <adegoketemitope1909@gmail.com>
2020-03-26 14:29:57 +0100

    modify syntax and format file

--- server/services/v1/auth/auth.js
+++ server/services/v1/auth/auth.js
@@ -4,120 +4,126 @@ const jwt = require("jsonwebtoken");
 const httpStatusCodes = require("http-status-codes");
 const chalk = require("chalk");
 
-const {
-    validationResult
-} = require("express-validator");
+const { validationResult } = require("express-validator");
 
 const userModel = require("../../../models/user");
 
 const register = async (req, res, next) => {
+  const errors = validationResult(req);
 
-    const errors = validationResult(req);
-
-    if (!errors.isEmpty())
-        return res.status(httpStatusCodes.UNPROCESSABLE_ENTITY).json({
-            errors: errors.array()
-        });
-
-    let {
-        name,
-        email,
-        password,
-        currency
-    } = req.body;
-
-    let isEmailExists = await userModel.findOne({
-        email
+  if (!errors.isEmpty())
+    return res.status(httpStatusCodes.UNPROCESSABLE_ENTITY).json({
+      errors: errors.array()
     });
 
-    if (isEmailExists)
-        return res.status(httpStatusCodes.CONFLICT).json({
-            errors: [{
-                msg: "email already exists"
-            }]
-        });
+  let { name, email, password, currency } = req.body;
 
-    let hashedPassword = await bcrypt.hash(password, 8);
+  let isEmailExists = await userModel.findOne({
+    email
+  });
 
-    try {
-        let user = await userModel.create({
-            name,
-            email,
-            password: hashedPassword,
-            currency
-        });
+  if (isEmailExists)
+    return res.status(httpStatusCodes.CONFLICT).json({
+      errors: [
+        {
+          msg: "email already exists"
+        }
+      ]
+    });
 
-        if (!user) throw new error();
+  let hashedPassword = await bcrypt.hash(password, 8);
 
-        return res.status(httpStatusCodes.CREATED).json({
-            success: [{
-                msg: "user registered successfully"
-            }]
-        });
-    } catch (error) {
-        console.log(chalk.red(error));
-        return res.status(httpStatusCodes.INTERNAL_SERVER_ERROR).json({
-            error: [{
-                msg: "there was a problem registering user"
-            }]
-        });
-    }
+  try {
+    let user = await userModel.create({
+      name,
+      email,
+      password: hashedPassword,
+      currency
+    });
+
+    if (!user) throw new error();
+
+    return res.status(httpStatusCodes.CREATED).json({
+      success: [
+        {
+          msg: "user registered successfully"
+        }
+      ]
+    });
+  } catch (error) {
+    console.log(chalk.red(error));
+    return res.status(httpStatusCodes.INTERNAL_SERVER_ERROR).json({
+      error: [
+        {
+          msg: "there was a problem registering user"
+        }
+      ]
+    });
+  }
 };
 
 const login = async (req, res, next) => {
+  const errors = validationResult(req);
 
-    const errors = validationResult(req);
+  if (!errors.isEmpty())
+    return res.status(httpStatusCodes.UNPROCESSABLE_ENTITY).json({
+      error: [
+        {
+          errors: errors.array()
+        }
+      ]
+    });
 
-    if (!errors.isEmpty())
-        return res.status(httpStatusCodes.UNPROCESSABLE_ENTITY).json({
-            error: [{
-                errors: errors.array()
-            }]
-        });
+  let { email, password } = req.body;
 
-    let {
-        email,
-        password
-    } = req.body;
+  try {
+    let isUserExist = await userModel.findOne({
+      email
+    });
+    let isPasswordValid = await bcrypt.compare(password, isUserExist.password);
 
-    try {
-        let isUserExist = await userModel.findOne({
-            email: email
-        });
-        let isPasswordValid = await bcrypt.compare(password, isUserExist.password);
+    if (!isUserExist || !isPasswordValid)
+      return res.status(httpStatusCodes.UNAUTHORIZED).json({
+        errors: [
+          {
+            msg: "invalid email/password"
+          }
+        ]
+      });
 
-        if (!isUserExist || !isPasswordValid)
-            return res.status(httpStatusCodes.UNAUTHORIZED).json({
-                errors: [{
-                    msg: "invalid email/password"
-                }]
-            });
+    let token = jwt.sign(
+      {
+        id: isUserExist._id
+      },
+      process.env.SECRET,
+      {
+        expiresIn: 86400
+      }
+    );
 
-        let token = jwt.sign({
-            id: isUserExist._id
-        }, process.env.SECRET, {
-            expiresIn: 86400
-        });
-
-        res.status(httpStatusCodes.OK).json({
-            success: [{
-                msg: "user login successful",
-                email: email,
-                token: token
-            }]
-        });
-    } catch (error) {
-        console.log(chalk.red(error));
-        return res.status(httpStatusCodes.INTERNAL_SERVER_ERROR).json({
-            error: [{
-                msg: "there was a problem login user in",
-                error
-            }]
-        });
-    }
+    res.status(httpStatusCodes.OK).json({
+      success: [
+        {
+          msg: "user login successful",
+          email: email,
+          token: token
+        }
+      ]
+    });
+  } catch (error) {
+    console.log(chalk.red(error));
+    return res.status(httpStatusCodes.INTERNAL_SERVER_ERROR).json({
+      error: [
+        {
+          msg: "there was a problem login user in",
+          error
+        }
+      ]
+    });
+  }
 };
 
 module.exports = {
-    register,
-    login
-}
+  register,
+  login
+};
