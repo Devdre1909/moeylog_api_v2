d06f80e75022dff64b68da302fce4bb506a13623
Adegoke Temitope <adegoketemitope1909@gmail.com>
2020-03-30 21:06:13 +0100

    add updateuserdetails put route

--- server/controllers/apis/v1/user.js
+++ server/controllers/apis/v1/user.js
@@ -3,11 +3,18 @@ const router = express.Router();
 
 const userService = require("../../../services/v1/user/user");
 const authClientRequest = require("../../../middlewares/authguard");
+const validator = require("../../../middlewares/validation");
 
 router.get(
   "/:userId",
   authClientRequest.authClientToken,
   userService.getUserDetails
+);
+router.put(
+  "/:userId",
+  authClientRequest.authClientToken,
+  validator.validateUpdateDetails(),
+  userService.updateUserDetails
 );
 
 module.exports = router;
--- server/services/v1/user/user.js
+++ server/services/v1/user/user.js
@@ -1,12 +1,15 @@
 const express = require("express");
 const userModel = require("../../../models/user");
-const httpStatusCode = require("http-status-codes");
+const httpStatusCodes = require("http-status-codes");
+const bcrypt = require("bcryptjs");
+const chalk = require("chalk");
+const { validationResult } = require("express-validator");
 
 const getUserDetails = async (req, res, next) => {
   let { userId } = req.params;
-  let user = await userModel.findById(userId).select("name, email, currency");
+  let user = await userModel.findById(userId).select("name currency email");
   if (!user) {
-    return res.status(httpStatusCode.NOT_FOUND).json({
+    return res.status(httpStatusCodes.NOT_FOUND).json({
       errors: [
         {
           msg: "no user found"
@@ -14,7 +17,7 @@ const getUserDetails = async (req, res, next) => {
       ]
     });
   }
-  return res.status(httpStatusCode.OK).json({
+  return res.status(httpStatusCodes.OK).json({
     success: [
       {
         msg: "user fetch successful",
@@ -24,6 +27,85 @@ const getUserDetails = async (req, res, next) => {
   });
 };
 
+const updateUserDetails = async (req, res, next) => {
+  const errors = validationResult(req);
+
+  if (!errors.isEmpty())
+    return res.status(httpStatusCodes.UNPROCESSABLE_ENTITY).json({
+      errors: errors.array()
+    });
+
+  let { userId } = req.params;
+  let { name, email, currency, password } = req.body;
+
+  let user = await userModel.findById(userId);
+
+  if (!user)
+    return res.status(httpStatusCodes.NOT_FOUND).json({
+      error: [
+        {
+          msg: "no user found"
+        }
+      ]
+    });
+
+  presentEmail = await userModel.findById(userId).select("email");
+
+  if (presentEmail.email != email) {
+    isEmailExist = await userModel.findOne({ email });
+
+    if (isEmailExist)
+      return res.status(httpStatusCodes.CONFLICT).json({
+        error: [
+          {
+            msg: "email exists"
+          }
+        ]
+      });
+  }
+
+  let hashedPassword;
+
+  if (password) hashedPassword = await bcrypt.hash(password, 8);
+
+  try {
+    user = await userModel.findByIdAndUpdate(
+      userId,
+      {
+        name,
+        email,
+        currency,
+        password: hashedPassword
+      },
+      {
+        new: true,
+        runValidators: true
+      }
+    );
+
+    if (!user) throw new Error();
+
+    return res.status(httpStatusCodes.CREATED).json({
+      success: [
+        {
+          msg: "user details updated",
+          user
+        }
+      ]
+    });
+  } catch (error) {
+    console.log(chalk.red(error));
+    return res.status(httpStatusCodes.INTERNAL_SERVER_ERROR).json({
+      error: [
+        {
+          msg: "there was a problem updating user details"
+        }
+      ]
+    });
+  }
+};
+
 module.exports = {
-  getUserDetails
+  getUserDetails,
+  updateUserDetails
 };
