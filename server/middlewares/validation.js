c71dd85db5e2087f16ece28de440042c1946dbd3
Adegoke Temitope <adegoketemitope1909@gmail.com>
2020-03-30 21:05:11 +0100

    correct typos, include validation for updating user details

--- server/middlewares/validation.js
+++ server/middlewares/validation.js
@@ -80,15 +80,50 @@ const validateTransactions = () => {
       .isLength({
         min: 12
       })
-      .withMessage("decriptions should be more that 12 letters"),
+      .withMessage("descriptions should be more that 12 letters"),
     check("can_edit")
       .exists()
       .withMessage("can edit is required")
   ];
 };
 
+const validateUpdateDetails = () => {
+  return [
+    check("name")
+      .exists()
+      .withMessage("name is required")
+      .isLength({
+        min: 3
+      })
+      .withMessage("name must be greater that 3 letters"),
+    check("email")
+      .exists()
+      .withMessage("email is required")
+      .isEmail()
+      .withMessage("email is invalid"),
+    check("password")
+      .exists()
+      .withMessage("password is required")
+      .isLength({
+        min: 8,
+        max: 18
+      })
+      .withMessage("password must be in between 8 to 12 characters long"),
+    check("currency")
+      .exists()
+      .withMessage("currency is required")
+      .isLength({
+        min: 3,
+        max: 3
+      })
+      .withMessage("currency must be 3 letters long")
+    // TODO Verify currency
+  ];
+};
+
 module.exports = {
   validateLoginBody,
   validateRegistrationBody,
-  validateTransactions
+  validateTransactions,
+  validateUpdateDetails
 };
